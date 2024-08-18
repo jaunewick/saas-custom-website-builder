f6f7a9304f76d7670f4a2778e571fc16a8a786bf
Daniel Giao <daniel.giao@icloud.com>
2024-08-17 23:03:20 -0400

    layout nested themeProvide tag fixed

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -14,9 +14,12 @@ export default function Home() {
           <h1 className="text-9xl font-bold text-center md:text-[200px]">Craftify</h1>
         </div>
         <div className="flex justify-center items-center relative md:mt-[-70px]">
-          <Image src={"/assets/preview.png"} alt="banner img" height={1200} width={1200} className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"/>
+          <Image src={"/assets/preview.png"} alt="banner img" height={1200} width={1200} className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted" />
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
+      </section>
+      <section className="flex justify-center items-center flex-col gap-4 md:mt-20">
+        <h2 className="text-4xl text-center">Choose what fits your right</h2>
       </section>
     </>
   );
